ac7c32eb554f99c605a51a59e5b8a1872b65c401
Sabari Raj N <106173910+Sabari004@users.noreply.github.com>
2024-01-01 18:56:47 +0530

    Time: 12 ms (10.01%), Space: 45.6 MB (6.13%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int i=0,j=0;
+        int n=g.length,m=s.length;
+        while(i<n&&j<m){
+            if(s[j]>=g[i]){
+                j++;
+                i++;
+            }
+            else{
+                j++;
+            }
+        }
+        return i;
+    }
+}
